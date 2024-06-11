3b6c94f4fc3a469e7ff9b163068fc176eb37da57
Rudinei Lopes <rudinei.lopes@hotmail.com>
2024-06-11 19:35:11 -0300

    defining product typing

--- next-commerce-shoes/src/app/page.tsx
+++ next-commerce-shoes/src/app/page.tsx
@@ -1,8 +1,28 @@
+import { ProductType } from "@/types/ProductType";
 
-export default function Home() {
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if (!res.ok) {
+    throw new Error('Failed fetching products')
+  }
+  return res.json();
+}
+
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
+  
+   
   return (
-    <div>
-      <h1 className="text-emerald-50">Hello my project</h1>
+    <div className="container mx-auto pt-8 px-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {products.map((product: ProductType)=> (
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden p-5" key={product.id}>
+            {product.title}
+          </div>
+        ) )}
+      </div>
     </div>
   );
 }
